7385fcb4619d6f1a2cdb868ded48db706c2591fe
KevinAlejandro17 <89487705+KevinAlejandro17@users.noreply.github.com>
2022-12-28 00:56:29 -0500

    si

--- ThunderApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from ThunderApp.views import UsersView
-
-urlpatterns=[
-    path('users/', UsersView.as_view(), name="users"),
-    path('users/<int:id>', UsersView.as_view(), name="user"),
-    path('users/verifyUser', UsersView.verifyUser, name="verify")
-]
