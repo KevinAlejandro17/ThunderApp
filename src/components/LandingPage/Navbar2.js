278d714f1f628c3da3a1f615a2aad3070c1d28e0
KevinAlejandro17 <89487705+KevinAlejandro17@users.noreply.github.com>
2022-12-31 02:55:47 -0500

    navbar updated

--- src/components/LandingPage/Navbar2.js
+++ src/components/LandingPage/Navbar2.js
@@ -7,6 +7,8 @@ import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 
+import logo from "./Images/logo3.png";
+
 function Navbar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -19,7 +21,7 @@ function Navbar() {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+            <img width="80" height="80" src={logo} alt="logo" />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             News
